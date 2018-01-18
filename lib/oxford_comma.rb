a197ec3c534ba76c34fb73f688d35203d1d1457f
Catherine Salazar <catcreates13@gmail.com>
2018-01-18 02:30:16 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -12,7 +12,8 @@ elsif array.length == 2
 else array.length == 3
   return "#{array[0]}, #{array[1]}, and #{array[2]}"
 
-else array.sort_by {|fruit| x.length}
+else array == 4
+  array.sort_by {|fruit| x.length}
 
 
 end
