3159d667d6d632b79c7992df72db4016b525afc9
Catherine Salazar <catcreates13@gmail.com>
2018-01-18 01:41:59 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,20 +2,5 @@
 
 def oxford_comma(array)
 
-if array.join do |fruit|
-
-end
-end
-
-elsif
-   array.join(" and ") do |fruit|
-
-end
-
-else
-  array.join(", ") do |fruit|
-end
-end
-
-
+if array.join { |fruit|  }
 end
