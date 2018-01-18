45c730d5c54c18037cb675944ccfe2d8080bcd27
Catherine Salazar <catcreates13@gmail.com>
2018-01-18 02:21:25 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -10,7 +10,7 @@ elsif array.length == 2
   return array.join(" and ")
 
 else array.length == 3
-  return array.join(", ") do |fruit| fruit[]-2] << (and)
+  return array.join(", ") do |fruit| fruit(-2) << (and)
   end
 
 end
