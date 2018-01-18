b8477d9bf42a90d8e88bf352478436494cca4a62
Catherine Salazar <catcreates13@gmail.com>
2018-01-18 02:52:52 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -13,7 +13,7 @@ elsif array.length == 3
   return "#{array[0]}, #{array[1]}, and #{array[2]}"
 
 else array.length > 3
-   array[-1] = array.join(", ") + array[-1]
+   array.join(", ") + array[-1]
 
 end
 
