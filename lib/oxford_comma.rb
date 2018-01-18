3e6545394803132c624696be953cb3938274ef67
Catherine Salazar <catcreates13@gmail.com>
2018-01-18 02:16:57 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,12 +4,12 @@ def oxford_comma(array)
 
 if array.length == 1
   return array.join
-end
+
 
 elsif array.length == 2
   array.join(" and ")
 
 else array.length == 3
-  array.join(" , ") 
+  array.join(" , ")
 
 end
