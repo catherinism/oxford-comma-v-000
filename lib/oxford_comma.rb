797f045792ea3fb92ea1438f5997ff12e3f946eb
Catherine Salazar <catcreates13@gmail.com>
2018-01-18 02:56:55 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -9,7 +9,7 @@ if array.length == 1
 elsif array.length == 2
   return array.join(" and ")
 
-elsif array.length == 3
+elsif array.length > 2
   return "#{array[0]}, #{array[1]}, and #{array[2]}"
 
 else array.length > 3
