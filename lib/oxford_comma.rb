a61aeb8e0d3d02b9c5159cfa6e71bc33817e4aad
Catherine Salazar <catcreates13@gmail.com>
2018-01-18 02:58:18 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -5,12 +5,12 @@ def oxford_comma(array)
 if array.length == 1
   array.join
 
-
 elsif array.length == 2
   array.join(" and ")
 
 elsif array.length == 3
-  "#{array[0]}, #{array[1]}, and #{array[2]}"
+  array.join(", ")
+  array[-1] = "and " + array[-1]
 
 else array.length > 3
   array[-1] = "and " + array[-1]
