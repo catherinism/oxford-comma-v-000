9b7667df112789af47c4a2ce1935fa9f2824422d
Catherine Salazar <catcreates13@gmail.com>
2018-01-18 02:58:43 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -8,9 +8,7 @@ if array.length == 1
 elsif array.length == 2
   array.join(" and ")
 
-elsif array.length == 3
-  array.join(", ")
-  array[-1] = "and " + array[-1]
+
 
 else array.length > 2
   array[-1] = "and " + array[-1]
