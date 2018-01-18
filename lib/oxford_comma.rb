12b98654c81f0dbe7102f9951c1b86ef84807b86
Catherine Salazar <catcreates13@gmail.com>
2018-01-18 02:36:35 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -13,7 +13,7 @@ elsif array.length == 3
   return "#{array[0]}, #{array[1]}, and #{array[2]}"
 
 else array.length == 4
-  return "#{array[0]}, #{array[1]}, #{array[2]}, and #{array[3]}"
+  return "#{array[0]}, #{array[1]}, #{array[2]}, #{array[3]}, and #{array[4]}"
 
 end
 
