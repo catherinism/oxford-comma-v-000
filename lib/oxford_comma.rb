7287e608cfe69b6602cc649fc39f40125d8e47d5
Catherine Salazar <catcreates13@gmail.com>
2018-01-18 02:35:09 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -12,8 +12,9 @@ elsif array.length == 2
 elsif array.length == 3
   return "#{array[0]}, #{array[1]}, and #{array[2]}"
 
-else
-  array.sort
+elsif array.length == 4
+  return "#{array[0]}, #{array[1]}, #{array[2]}, #{array[3]}"
+
 end
 
 end
