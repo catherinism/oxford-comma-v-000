045ffe3cd281ed2357f9716c44c9ae481be53433
Catherine Salazar <catcreates13@gmail.com>
2018-01-18 02:25:45 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -10,8 +10,8 @@ elsif array.length == 2
   return array.join(" and ")
 
 else array.length == 3
-  return "#{array[0], array[1], and array[2]}"
-  
+  return "#{array[0]}, #{array[1]} and #{array[2]}
+
 end
 
 end
